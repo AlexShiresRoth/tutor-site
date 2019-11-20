105f59503f15ca66d6634836fabfa210c384cf52
Alex Roth <alexroth96@gmail.com>
2019-11-20 13:16:18 -0500

    more header styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,9 @@
 import React from "react"
 import PropTypes from "prop-types"
 import headerStyles from "./headerstyles/header.module.scss"
+import { FiVideo, FiUsers } from "react-icons/fi"
+import { FaBookOpen } from "react-icons/fa"
+import { TiDeviceLaptop } from "react-icons/ti"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Header = () => {
@@ -18,16 +21,24 @@ const Header = () => {
     <header className={headerStyles.header}>
       <div className={headerStyles.text__box}>
         <h1>{data.site.siteMetadata.title}</h1>
+        <div className={headerStyles.divider}>
+          <span></span>
+          <FaBookOpen />
+          <span></span>
+        </div>
         <h2>Willy Williams Tutoring</h2>
       </div>
       <div className={headerStyles.services__grid}>
         <div className={headerStyles.item}>
+          <FiUsers />
           <p>In Person Tutoring</p>
         </div>
         <div className={headerStyles.item}>
+          <TiDeviceLaptop />
           <p>Virtual Tutoring</p>
         </div>
         <div className={headerStyles.item}>
+          <FiVideo />
           <p>Video Conference</p>
         </div>
       </div>
