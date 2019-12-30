474cc6b9d7557dbbb535f353db845b97afaba1f8
Alex Rothenberg <alexroth96@gmail.com>
2019-12-30 18:42:53 -0500

    fix active state for quiz questions, add progress bar

--- src/components/quiz/QuizMap.js
+++ src/components/quiz/QuizMap.js
@@ -12,22 +12,16 @@ class QuizMap extends React.Component {
     super(props)
     this.state = {
       answers: [],
-      currentIndex: 0,
+      currentQuestionIndex: 0,
+      activeIndex: null,
+      progressBar: 0,
+      questions: null,
     }
   }
 
-  setAnswerState = e => {
+  setAnswerState = (e, index) => {
     //add selected state to answer
     e.persist()
-    e.target.childNodes[0].type === "radio"
-      ? (e.target.childNodes[0].checked = true)
-      : (e.target.parentNode.childNodes[0].checked = true)
-
-    const inputs = [...e.target.parentNode.childNodes]
-
-    inputs
-      .filter((input, i) => input.checked)
-      .map(node => node.parentNode.classList.add(this.props.quizStyles.active))
 
     const name = e.target.childNodes[0].name
       ? e.target.childNodes[0].name
@@ -51,8 +45,35 @@ class QuizMap extends React.Component {
     })
   }
 
+  //set active class for quiz choices
+  setActiveState = (e, index) => {
+    e.persist()
+    e.target.childNodes[0].type === "radio"
+      ? (e.target.childNodes[0].checked = true)
+      : (e.target.parentNode.childNodes[0].checked = true)
+
+    this.setState({
+      activeIndex: index,
+    })
+  }
+
+  componentDidMount() {
+    const { quiz } = this.props
+    this.setState({
+      questions: quiz.length,
+    })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.answers.length !== prevState.answers.length) {
+      this.setState({
+        progressBar: (this.state.answers.length / this.state.questions) * 100,
+      })
+    }
+  }
+
   render() {
-    console.log(this.state.answers)
+    console.log(this.state)
     const { quiz, quizStyles } = this.props
 
     const quizMap = quiz.map((ques, i) => {
@@ -72,15 +93,21 @@ class QuizMap extends React.Component {
             {ques.choices.map((choice, i) => {
               return (
                 <div
-                  className={quizStyles.input__row}
-                  onClick={e => this.setAnswerState(e)}
+                  className={`${quizStyles.input__row} ${
+                    this.state.activeIndex === i ? quizStyles.active : ""
+                  }`}
+                  onClick={e => {
+                    this.setAnswerState(e)
+                    this.setActiveState(e, i)
+                  }}
+                  key={i}
                 >
                   <input
                     type="radio"
                     name={ques.name}
                     value={choice.qNum}
                   ></input>
-                  <label for="answer">
+                  <label for={choice.qNum}>
                     {choice.qNum}: {choice.ans}
                   </label>
                 </div>
@@ -90,8 +117,15 @@ class QuizMap extends React.Component {
         </div>
       )
     })
+
     return (
       <>
+        <div className={quizStyles.progress__bar}>
+          <div
+            className={quizStyles.progress__bar__fill}
+            style={{ width: `${this.state.progressBar}%` }}
+          ></div>
+        </div>
         <div className={quizStyles.quiz}>{quizMap}</div>
         <button disabled>Submit Answers</button>
       </>
