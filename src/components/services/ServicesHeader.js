710c29c0f90973ec7be8e6ccd38204d5e25e1525
Alex Rothenberg <alexroth96@gmail.com>
2020-01-05 19:53:19 -0500

    fix up responsive design

---
+++ src/components/services/ServicesHeader.js
@@ -0,0 +1,10 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const ServicesHeader = props => {
+  return <header></header>
+}
+
+ServicesHeader.propTypes = {}
+
+export default ServicesHeader
--- src/pages/services.js
+++ src/pages/services.js
@@ -2,14 +2,14 @@ import React from "react"
 import PropTypes from "prop-types"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
+import ServicesHeader from "../components/services/ServicesHeader"
 const services = props => {
-	return (
-		<Layout>
-			<SEO title="Services" />
-			<h1>services</h1>
-		</Layout>
-	)
+  return (
+    <Layout>
+      <SEO title="Services" />
+      <ServicesHeader />
+    </Layout>
+  )
 }
 
 services.propTypes = {}
